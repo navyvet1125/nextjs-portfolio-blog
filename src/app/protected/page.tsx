1977d0d16eb157b837cf75d78ef785a17be681d0
Evan Washington <enavy04@gmail.com>
2024-07-13 00:19:57 -0700

    switched from jwts to sessions and role to session

---
+++ src/app/protected/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Protected = () => {
+  return (
+    <div>
+        <h1>Protected Page</h1>
+        <p>This is a protected page</p>
+    </div>
+  )
+}
+
+export default Protected
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,14 +1,53 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth" // Import the NextAuthUser type
 import { PrismaAdapter } from "@auth/prisma-adapter"
 // import { PrismaClient } from "@prisma/client"
 import { Adapter } from "next-auth/adapters"
 import prisma from "@/lib/db"
 import authConfig from "./auth.config"
+import { Session } from "next-auth"
 
-// const prisma = new PrismaClient()
+// Extend the existing User type
+
+// Extend the Session model to include the role in the user object
+interface ExtendedSession extends Session {
+  user: {
+    id: string;
+    name?: string;
+    email?: string;
+    image?: string;
+    role?: string;
+  };
+}
+
+async function getUserRole(userId: string) {
+  const user = await prisma.user.findUnique({
+    where: { id: userId },
+    select: { role: true },
+  })
+  return user?.role;
+}
 
 export const {auth, handlers, signIn, signOut} = NextAuth({
   adapter: PrismaAdapter(prisma) as Adapter,
-  session: { strategy: "jwt" },
+  // session: { strategy: "jwt" },
   ...authConfig,
-})
+  events: {
+    signIn: async ({ user, isNewUser }) => {
+      if (isNewUser && user.email) {
+        if (user.email === process.env.ADMIN_EMAIL) {
+          await prisma.user.update({
+            where: { email: user.email },
+            data: { role: "ADMIN" },
+          })
+        }
+      }
+    }
+  },
+  callbacks: {
+    session: async ({ session, user }) => {
+      const extendedSession = session as ExtendedSession
+      extendedSession.user.role = await getUserRole(user.id)
+      return extendedSession;
+    },
+  },
+});
